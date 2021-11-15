a846fcca0ac76862e58f31cb0b24cbcbf68b401b
maxlivit <maxim.livit22@sitechhs.com>
2021-11-15 12:28:26 -0500

    cmon nika

---
+++ hello-world/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+<script> 
+export default {
+  data(){
+    
+  },
+  methods: {
+     async apiCall() {
+        fetch('https://api.adoptapet.com/search/pets_at_shelter?key=A34F48&v=1&output=xml&shelter_id=2342')
+            .then(data => {
+               console.log(data);
+              return data.json();
+    })
+     } 
+  },
+  mounted() {
+    this.apiCall;
+  },
+}
+
+
+</script> 
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
