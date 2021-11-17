311807c3dc0034442b6de43811a1a7b376f1e79b
nika2004 <nika.imamberdieva22@sitechhs.com>
2021-11-17 12:27:12 -0500

    11/17/21

---
+++ vue-dictionary/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home"> 
+    <WordList/>
+  </div>
+</template>
+
+<script>
+import WordList from "../components/WordList.vue"
+
+export default {
+  name: "Home",
+  components: {
+    WordList,
+  },
+};
+</script>
