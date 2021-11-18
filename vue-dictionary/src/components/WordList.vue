8a3df0cb5b6d8836349e91796605fc52232861d6
nika2004 <nika.imamberdieva22@sitechhs.com>
2021-11-18 12:33:23 -0500

    11/18/21

--- vue-dictionary/src/components/WordList.vue
+++ vue-dictionary/src/components/WordList.vue
@@ -1,12 +1,39 @@
 <template>
   <div>
-      <h2>TEST</h2>
+      <h1>TEST</h1>
+      <ul class="word-table">
+        <li class="word-list-item" v-for="word in words" :key="word.name">{{ word.name }}</li>
+      </ul>
+
   </div>
 </template>
 
 <script>
 export default {
     name: "WordList",
+    data(){
+      return{
+        words:["hello"]
+      };
+    },
+    props: ["word"],
+    created: function (){
+      this.fetchData();
+    },
+    methods: {
+      fetchData: async function(){
+        try{
+          const result = await fetch(
+            `https://api.dictionaryapi.dev/api/v2/entries/en/${this.words}`
+          );
+          const data = await result.json();
+          console.log(data);
+          this.words = data.reults;
+        } catch (error){
+          alert(error);
+        }
+      }
+    },  
 };
 </script>
 
